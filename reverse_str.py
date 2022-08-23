64c144b3d1c7006c2aefb5ae06858d2be863c114
Jaskirat Singh <101934580+JaskiratSingh08@users.noreply.github.com>
2022-08-23 22:47:42 +0530

    Update reverse_str.py

--- reverse_str.py
+++ reverse_str.py
@@ -1,8 +1,13 @@
-def reverseString(testString):
-  str = ""
-  for i in testString:
-    str = i + str
-  return str
- 
-testString = input()
-print(reverseString(testString))
+def reverseString(testString):                # creating a new function called reverseString whivh takes testString as argument
+  str = ""                                    # initialised a variable called str which is empty
+  for i in testString:                        # a loop which will run upto length of our testString value
+    str = i + str                             # iterating each character and joing it to make our final reverse string
+  return str                                  # to return the str value when the fuction is called and finished executing
+
+  # or instead of writting above four lines, just:
+  # str = testString[::-1]                                                # created a variable str, we iterate through our testString from last character and
+                                                                          # asign the value to str after iterating
+  
+
+testString = input()                          # testString is getting assigned the value which is taken from user as input
+print(reverseString(testString))              #calling the function reverseString by pssing the input value and printing the output.
